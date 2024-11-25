c8fb1d0d713eccbe9aca25b7cdefec3502e2df38
siva <siva26sakthi05@gmail.com>
2024-11-25 17:36:14 +0530

    use function

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+int sum(int a,int b){
+    return a+b;
+}
 using namespace std;
 int main(){
     int a,b,c;
     cin>>a>>b;
-    c=a+b;
+    c=sum(a,b);
     cout<<c;
     return 0;
 
